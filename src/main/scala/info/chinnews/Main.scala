c8b167d7abab934fb6f9741ee711688ed536c3ea
Andrey Tsarevskiy <andreytsarevskiy@gmail.com>
2015-11-15 09:29:03 +0100

    resslient system

--- src/main/scala/info/chinnews/Main.scala
+++ src/main/scala/info/chinnews/Main.scala
@@ -1,5 +1,6 @@
 package info.chinnews
 
+import java.io.File
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
@@ -13,7 +14,12 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    val conf = ConfigFactory.load()
+    val applicationFile = new File("./application.conf")
+    val conf = if (applicationFile.exists()) {
+      ConfigFactory.parseFile(applicationFile)
+    } else {
+      ConfigFactory.load()
+    }
 
     val actorSystem = ActorSystem()
     val scheduler = actorSystem.scheduler
@@ -21,18 +27,18 @@ object Main {
     InstragramAuth(conf.getString("instagram.client_id"), conf.getString("instagram.client_secret"))
       .auth("", "", (accessToken, failureListener) => {
 
-      val db = DB(conf.getString("db.name"), conf.getString("db.host"), conf.getInt("db.port"))
-      scheduler.schedule(
-        initialDelay = Duration(5, TimeUnit.SECONDS),
-        interval = Duration(60, TimeUnit.SECONDS),
-        runnable = new LocationCrawler(accessToken, failureListener, db))
+        val db = DB(conf.getString("db.name"), conf.getString("db.host"), conf.getInt("db.port"))
+        scheduler.schedule(
+          initialDelay = Duration(5, TimeUnit.SECONDS),
+          interval = Duration(60, TimeUnit.SECONDS),
+          runnable = new LocationCrawler(accessToken, failureListener, db))
 
-      scheduler.schedule(
-        initialDelay = Duration(5, TimeUnit.SECONDS),
-        interval = Duration(20, TimeUnit.SECONDS),
-        runnable = new TagCrawler(accessToken, failureListener, db))
-    }
-    )
+        scheduler.schedule(
+          initialDelay = Duration(5, TimeUnit.SECONDS),
+          interval = Duration(20, TimeUnit.SECONDS),
+          runnable = new TagCrawler(accessToken, failureListener, db))
+      }
+      )
   }
 }
 
