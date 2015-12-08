73cffd735d67ddd609ad4609490e3ed60f18c4c5
Andrey Tsarevskiy <andreytsarevskiy@gmail.com>
2015-12-08 01:12:30 +0100

    screenshots

--- src/main/resources/instagram_login.js
+++ src/main/resources/instagram_login.js
@@ -18,7 +18,7 @@ page.open(url, function (status) {
     if (status !== 'success') {
         console.log('Unable to access network');
     } else {
-        page.render('/root/screenshot.png')
+        page.render('/root/test/screenshot.png');
         console.log('Slimerjs: Clicking to button');
         page.evaluate(function (args) {
             document.getElementById("id_username").value = args[4];
