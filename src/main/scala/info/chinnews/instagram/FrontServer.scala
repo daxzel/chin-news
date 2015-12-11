bd209dbdc8be85e4f5ecc7ecea37cc92a3bff7e3
Andrey Tsarevskiy <andreytsarevskiy@gmail.com>
2015-12-11 23:49:48 +0100

    actors fix

--- src/main/scala/info/chinnews/instagram/FrontServer.scala
+++ src/main/scala/info/chinnews/instagram/FrontServer.scala
@@ -17,6 +17,7 @@ object FrontServer {
 
   def subscribe(system: ActorSystem) {
     logger.info("Subscribing to the port: " + PORT)
+    val instagramMediaActor = system.actorOf(Props[InstagramMediaActor], "instagramMediaActor")
 
     Executors.newSingleThreadExecutor().execute(new Runnable {
       override def run(): Unit = {
@@ -35,50 +36,54 @@ object FrontServer {
           server.configureBlocking(false)
           val serverKey = server.register(selector, SelectionKey.OP_ACCEPT)
           while (true) {
-            selector.select()
-            val keys = selector.selectedKeys()
-            val iterator = keys.iterator()
-            while (iterator.hasNext) {
-              val key = iterator.next()
-              logger.info("Received a key: " + key.toString)
-              iterator.remove()
+            try {
+              selector.select()
+              val keys = selector.selectedKeys()
+              val iterator = keys.iterator()
+              while (iterator.hasNext) {
+                val key = iterator.next()
+                logger.info("Received a key: " + key.toString)
+                iterator.remove()
 
-              if (key == serverKey) {
-                if (key.isAcceptable) {
-                  val client: SocketChannel = server.accept()
-                  client.configureBlocking(false)
-                  val clientKey = client.register(selector, SelectionKey.OP_READ)
-                  clientKey.attach(0)
-                }
-              } else {
-                val client = key.channel().asInstanceOf[SocketChannel]
-                if (key.isReadable) {
-                  val bytesRead = client.read(buffer)
-                  if (bytesRead == -1) {
-                    key.cancel()
-                    client.close()
-                  } else {
-                    buffer.flip()
-                    val request = decoder.decode(buffer).toString
-                    logger.info(request)
-                    buffer.clear()
-
-                    val index = request.indexOf("hub.challenge")
-                    if (index > 0) {
-                      val startIndex = index + 14
-                      val endIndex = startIndex + 32
-                      val hub_challenge = request.substring(startIndex, endIndex)
-                      logger.info("Calculated hub_challenge: " + hub_challenge)
-                      client.write(encoder.encode(CharBuffer.wrap(hub_challenge)))
-                    } else {
-                      system.actorOf(Props[InstagramMediaActor], "instagramMediaActor") ! request
-                    }
-                    key.cancel()
-                    client.close()
+                if (key == serverKey) {
+                  if (key.isAcceptable) {
+                    val client: SocketChannel = server.accept()
+                    client.configureBlocking(false)
+                    val clientKey = client.register(selector, SelectionKey.OP_READ)
+                    clientKey.attach(0)
                   }
+                } else {
+                  val client = key.channel().asInstanceOf[SocketChannel]
+                  if (key.isReadable) {
+                    val bytesRead = client.read(buffer)
+                    if (bytesRead == -1) {
+                      key.cancel()
+                      client.close()
+                    } else {
+                      buffer.flip()
+                      val request = decoder.decode(buffer).toString
+                      logger.info(request)
+                      buffer.clear()
 
+                      val index = request.indexOf("hub.challenge")
+                      if (index > 0) {
+                        val startIndex = index + 14
+                        val endIndex = startIndex + 32
+                        val hub_challenge = request.substring(startIndex, endIndex)
+                        logger.info("Calculated hub_challenge: " + hub_challenge)
+                        client.write(encoder.encode(CharBuffer.wrap(hub_challenge)))
+                      } else {
+                        instagramMediaActor ! request
+                      }
+                      key.cancel()
+                      client.close()
+                    }
+
+                  }
                 }
               }
+            } catch {
+              case e: Exception => logger.error(e.toString)
             }
           }
         } catch {
